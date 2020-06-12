65dbc794936c18e624174ec6f66b5b72ed7b05fa
j-j-j-j <65978591+j-j-j-j@users.noreply.github.com>
2020-06-13 07:10:51 +0900

    関数分割、整理

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let CHARS = ' .,\'"\\:;\\-=_|＼／<>';
 
-function escapeToUnicode() {
+function main() {
     let s = document.getElementById('input').value;
+    escapeToUnicode(s);
+    clipboard(s);
+    checkByteCount(s);
+}
 
+function escapeToUnicode() {
     let r = /[　 \n][　 ]{3}[　 \n]/;
     while (r.test(s)) {
         s = avoid(s, r);
@@ -14,53 +19,6 @@ function escapeToUnicode() {
     }
 
     console.log(s)
-
-    const tempElement = document.createElement('textarea');
-    tempElement.style = "position: absolute; left: -9999px; top: -9999px";
-    tempElement.value = s;
-    document.body.appendChild(tempElement);
-    tempElement.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempElement);
-
-    const result = document.getElementById('result');
-    result.textContent = 'クリップボードにコピーされました';
-    result.className = '';
-
-    // 行数ごとのバイト数チェック
-    // （出力ではされなくとも）HTMLエスケープされてからチェックにかけられる
-    // UTF-8ではなくShift_JISでカウントされる
-    let overflowedRow;
-    let overflowedBytes = 0;
-
-    s
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .split('\n')
-        .some((s, i) => {
-            let bytes = 0;
-            for (let c of s) {
-                let code = c.charCodeAt();
-                if ((code >= 0x0 && code < 0x81) || (code >= 0xff61 && code < 0xffa0) || (code >= 0xf8f0 && code < 0xf8f4)) {
-                    bytes += 1;
-                } else {
-                    bytes += 2;
-                }
-            }
-            if (bytes >= 256) {
-                overflowedRow = i + 1;
-                overflowBytes = bytes - 255;
-                return true;
-            } else {
-                return false;
-            }
-        });
-    if (overflowedRow) {
-        result.textContent = `${overflowedRow}行目が${overflowBytes}バイト超過 連続するスペースや半角記号を減らしてください`;
-        result.className = 'error';
-    }
 
     function avoid(s, r) {
         return s.replace(r, (sub) => {
@@ -79,3 +37,51 @@ function escapeToUnicode() {
     }
 }
 
+function clipboard(s) {
+    const tempElement = document.createElement('textarea');
+    tempElement.style = "position: absolute; left: -9999px; top: -9999px";
+    tempElement.value = s;
+    document.body.appendChild(tempElement);
+    tempElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempElement);
+
+    const result = document.getElementById('result');
+    result.textContent = 'クリップボードにコピーされました';
+    result.className = '';
+}
+
+function checkByteCount(s) {
+    // バイト数チェック
+    // （出力ではされなくとも）HTMLエスケープされてからチェックにかけられる
+    // UTF-8ではなくShift_JISでカウントされる
+
+    s
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .split('\n')
+        .some((s, i) => {
+            let bytes = 0;
+            for (let c of s) {
+                let code = c.charCodeAt();
+                if ((code >= 0x0 && code < 0x81) || (code >= 0xff61 && code < 0xffa0) || (code >= 0xf8f0 && code < 0xf8f4)) {
+                    bytes += 1;
+                } else {
+                    bytes += 2;
+                }
+            }
+            if (bytes >= 256) {
+                const overflowedRowNum = i + 1;
+                const overflowBytes = bytes - 255;
+
+                result.textContent = `${overflowedRowNum}行目が${overflowBytes}バイト超過 連続するスペースや半角記号を減らしてください`;
+                result.className = 'error';
+
+                return true;
+            } else {
+                return false;
+            }
+        });
+}
